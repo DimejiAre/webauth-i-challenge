7362d4245b229e5c1b97fefcc09ef0ae321d2000
DimejiAre <olardi2@gmail.com>
2019-11-12 15:24:42 +0100

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+function App() {
+
+  const [user, setUser] = useState('');
+
+  const login = (formValue, actions) => {
+    console.log(formValue)
+    const newUser = {
+      name: formValue.name,
+      password: formValue.password,
+    }
+
+    axios.post('http://localhost:4000/api/auth/login', newUser)
+    .then(res => {
+      setUser(res.data);
+      actions.resetForm();
+    })
+    .catch(err => {
+      alert(err.message)
+    })
+  }
+
+  return (
+    <div className="App">
+      <LoginForm login={login}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Formik, Form, Field} from 'formik';
+
+function LoginForm(props){
+    const {login} = props;
+    const initialUser = {username: '', password: ''}
+    return(
+        <Formik 
+        initialValues = {initialUser}
+        onSubmit={login}
+        render={props => {
+            return(
+                <Form>
+                    <label htmlFor='username'>username</label>
+                    <Field name='username' type='text' placeholder='username'/>
+                    <label htmlFor='password'>username</label>
+                    <Field name='password' type='password' placeholder='password'/>
+                    <button type='submit'>Log In</button>
+                </Form>
+            )
+        }}/>
+    )
+
+}
+
+export default LoginForm;
